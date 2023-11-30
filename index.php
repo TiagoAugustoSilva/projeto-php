2ba70712e6e58ad26001444e51e0ebacfc107b6a
tiagoaugusto <112525927+TiagoAugustoSilva@users.noreply.github.com>
2023-11-30 12:18:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 require_once("conexao.php");
 
-$results = $conn->query("select * from cadastrar_produtos")->fetchAll();
+$results = $conn->query("select * from cadastrar_produto")->fetchAll();
 
-include_once("layout/_header.php");
+$arrayDescricao = [1 => 'Eletrônicos', 2 => 'Informática', 3 => 'Domésticos', 4 => 'Celulares', 5 => 'Acessórios'];
+
+include_once("./layout/_header.php");
 
 ?>
 
@@ -12,7 +14,7 @@ include_once("layout/_header.php");
 
 require_once("conexao.php");
 
-include_once("layout/_header.php");
+include_once("./layout/_header.php");
 
 ?>
 
@@ -20,8 +22,9 @@ include_once("layout/_header.php");
 
 <div class="card mt-4">
     <!--justify-content-between joga o botão la no fim Chick D+-->
-    <div class="card-header d-flex justify-content-between align-items-center">
-        <h5>Mini Faturamento</h5>
+    <div class="card-header d-flex justify-content-between align-items-center custom-card-header">
+        <!-- o text-center my-4 mx-auto é o mesmo que display flex, justify-content-center align-items-center-->
+        <h5 class="text-center my-4 h5-striped custom-h5">Mini Faturamento</h5>
         <!--ao utilizar btn-success ele dará a  cor verde ao botão pelo boostrap-->
         <a class="btn btn-success" href="cadastro.php">Adicionar</a>
     </div>
@@ -38,16 +41,19 @@ include_once("layout/_header.php");
                 </tr>
             </thead>
             <tbody>
-                <?php foreach($results as $item): ?>
+                <?php foreach ($results as $item) : ?>
                     <tr>
-                          <td><?= $item['produto']?></td>
-                          <td><?= $item['descricao']?></td>
-                          <td>
+                        <td><?= $item['produto'] ?></td>
+                        <td><?= $arrayDescricao[$item['descricao']] ?></td>
+                        <td><?= $item['valor_unitario'] ?></td>
+                        <td><?= $item['unidade_medida'] ?></td>
+                        <!--área abaixo onde colocamos os nosso btn(Buttons) lembrando que btn-primary botão azul e btn-danger-botão vermelho-->
+                        <td><a class="btn btn-sm btn-primary" href="cadastro.php?id=<?= $item['id']?>">Editar</a>
+                        <button class="btn btn-sm btn-danger">Excluir</button>
+                        </td>
 
-                          </td>
-                        
                     </tr>
-                    <?php endforeach; ?>
+                <?php endforeach; ?>
             </tbody>
         </table>
 
@@ -55,11 +61,11 @@ include_once("layout/_header.php");
 </div>
 
 <?php
-include_once("layout/_footer.php");
+include_once("./layout/_footer.php");
 ?>
 
 
 
 
 <?php
-include_once("layout/_footer.php"); ?>
+include_once("./layout/_footer.php"); ?>
